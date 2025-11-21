7680ed9f3fb198b1de870d93be69af06f2e40021
Shabin894396 <2004shabin@gmail.com>
2025-11-21 22:00:31 +0530

    change shopNow

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,7 +63,7 @@ const Index = () => {
             href="#products"
             className="inline-block bg-white text-primary font-semibold py-3 px-6 rounded-lg shadow hover:bg-gray-200 transition"
           >
-            Shop Now
+            NextBuy
           </a>
         </div>
       </section>
